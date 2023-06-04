cd01a406af037241119fdf3f95c3dfe77f650caa
Victor JEMAN <victorjeman@gmail.com>
2023-06-04 12:59:13 +0300

    Impement actor sorting

--- src/features/actor/components/actor-list/actor-list.tsx
+++ src/features/actor/components/actor-list/actor-list.tsx
@@ -1,22 +1,53 @@
-import { Grid, Paper, Stack, Text } from '@mantine/core';
+import { useMemo, useState } from 'react';
+import { Button, Grid, Paper, SegmentedControl, Stack, Text } from '@mantine/core';
 
 import { useGetActorsQuery } from 'features/actor/api/actor-api';
 import { ActorCreate } from 'features/actor/components/actor-create/actor-create';
 import { ActorThumbnail } from 'features/actor/components/actor-thumbnail/actor-thumbnail';
+import { Actor } from 'features/actor/types/actor-types';
 
 export const ActorList = () => {
   const { data: actors } = useGetActorsQuery();
+  const [sortType, setSortType] = useState<'ascending' | 'descending' | 'default'>('default');
+
+  function sortActors() {
+    if (actors) {
+      const actorsClone: Actor[] = JSON.parse(JSON.stringify(actors));
+
+      return actorsClone?.sort((a, b) => {
+        if (sortType === 'ascending') return a.name.localeCompare(b.name);
+        if (sortType === 'descending') return b.name.localeCompare(a.name);
+        return 1;
+      });
+    }
+
+    return actors;
+  }
+
+  const sortedActors: Actor[] | undefined = useMemo(sortActors, [actors, sortType]);
 
   return (
     <>
+      <Text size="lg" mb="sm">
+        Sort the actors
+      </Text>
+      <SegmentedControl
+        onChange={(value: 'ascending' | 'descending' | 'default') => setSortType(value)}
+        defaultValue="default"
+        data={[
+          { label: 'default', value: 'default' },
+          { label: 'ascending', value: 'ascending' },
+          { label: 'descending', value: 'descending' },
+        ]}
+      />
+
       <Grid gutter="2rem" my="xl">
-        {actors?.map((actor) => (
+        {sortedActors?.map((actor) => (
           <Grid.Col span={4} key={`actor-${actor.id}`}>
             <ActorThumbnail actor={actor} />
           </Grid.Col>
         ))}
       </Grid>
-
       {actors?.length === 0 && (
         <Paper shadow="md" withBorder p="xl" maw={600} mx="auto">
           <Stack align="center">
@@ -28,7 +59,6 @@ export const ActorList = () => {
           </Stack>
         </Paper>
       )}
-
       {actors?.length !== 0 && <ActorCreate />}
     </>
   );
